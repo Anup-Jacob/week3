c5433b6ffc4c3fd6c11ee0e5b5f4c218b874fbd0
Anup Jacob <L00163455@student.lyit.ie>
2021-10-08 10:02:23 +0100

    Python variable code

---
+++ python_variables.py
@@ -0,0 +1,10 @@
+# ---------------------------------
+
+# File          : python_variables.py
+# Author        : Anup Jacob
+# Version       : v1.0
+# Created Date  : 08/10/2021
+# Modified Date : 
+# Description   : Testing the functionality of List, Tuples
+# Licensing     : Anup Jacob, LYIT
+# ----------------------------------
