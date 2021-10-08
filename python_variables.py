e78d5ee843d8752c6ba5e5f0c37c8a8fb53c61bb
Anup Jacob <L00163455@student.lyit.ie>
2021-10-08 10:32:00 +0100

    Python variable code

--- python_variables.py
+++ python_variables.py
@@ -8,3 +8,47 @@
 # Description   : Testing the functionality of List, Tuples
 # Licensing     : Anup Jacob, LYIT
 # ----------------------------------
+
+if __name__ == "__main__":
+    tuple_test = ("L12345", "L54321")
+
+    # x = input("Enter the L-Number1 : ")
+
+    # print(" The L-number you have entered is : "+x)
+
+    # y = input("Enter the L-number2 : ")
+
+    # print(" The L-number you have entered is : " + y)
+
+    Student1_details = [x,"Java_ooprogramming","Python_Scripting"]
+    Student2_details = [y, "Java_ooprogramming", "Python_Scripting"]
+
+    print("The details of Student 1 is ")
+    print(Student1_details)
+    print("The details of Student 2 is ")
+    print(Student2_details)
+
+
+
+print("Thank you for using my application")
+'''
+
+   Main method of application 
+
+
+
+   Linear programming only presented here wrt demo of lists
+
+
+
+   Parameters:
+
+    none
+
+
+
+   Returns:
+
+    none
+
+  '''
