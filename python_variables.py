910862e8d814624de647a86294715260ff719158
Anup Jacob <L00163455@student.lyit.ie>
2021-10-08 11:04:57 +0100

    Python variable code

--- python_variables.py
+++ python_variables.py
@@ -12,13 +12,43 @@
 if __name__ == "__main__":
 
     tuple_test = ("L12345", "L54321")
+    # Tuple list of 2 fake Lnumbers for students
     module_list = ["Java_ooprogramming","Python_Scripting"]
+    # List of 2 module subjects to be assigned to the students
 
     print(tuple_test)
     print(module_list)
 
+    # Printing tuple lists for check
+
     student1_details = {"L12345":module_list}
     student2_details = {"L54321":module_list}
+    # Dictionary for L number and module list
+
+    java_stud1 = {"L12345": 40}
+    java_stud2 = {"L54321": 70}
+    # Dictionary for L number and Java grade for both students
+
+    python_stud1 = {"L12345": 69}
+    python_stud2 = {"L54321": 58}
+    # Dictionary for L number and python grade for both students
+
+    print("\n")
+
+
+    print("Student 1 grades : ")
+    print(java_stud1)
+    print(python_stud1)
+
+    # Printing grade details of Student 1
+
+    print("\n")
+
+    # Printing grade details of Student 2
+
+    print("Student 2 grades : ")
+    print(java_stud2)
+    print(python_stud2)
 
     print(student1_details)
     print(student2_details)
@@ -41,7 +71,7 @@ if __name__ == "__main__":
     #print(Student2_details)
 
 
-
+    print("\n")
 print("Thank you for using my application")
 '''
 
